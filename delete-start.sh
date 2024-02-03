06cb613121ebc63502954d4e38a93d2575b9f80a
CarmenTrual <carmen.trujillo.alvarez@hotmail.com>
2024-02-03 18:28:31 +0100

    TomCat

--- delete-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Eliminar la pila
-aws cloudformation delete-stack \
---stack-name TomCatPrueba  \
-# Descomenta la linea de abajo si quieres preservar las propiedades
-# --preserve-stack-properties
